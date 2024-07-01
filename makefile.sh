a69e0c2439a150380b89d354a66e1586dfa039fa
gitricko <gitricko@users.noreply.github.com>
2024-06-30 19:41:44 -0700

    Upgrade SonarQube Server to 10.6-community (#23)

--- makefile.sh
+++ makefile.sh
@@ -10,7 +10,7 @@ export SONAR_METRICS_PATH=${SONAR_METRICS_PATH:-"./sonar-metrics.json"}
 export SONAR_EXTENSION_DIR="${HOME}/.sonarless/extensions"
 
 export DOCKER_SONAR_CLI=${DOCKER_SONAR_CLI:-"sonarsource/sonar-scanner-cli:10.0"}
-export DOCKER_SONAR_SERVER=${DOCKER_SONAR_SERVER:-"sonarqube:10.5-community"}
+export DOCKER_SONAR_SERVER=${DOCKER_SONAR_SERVER:-"sonarqube:10.6-community"}
 
 export CLI_NAME="sonarless"
 
